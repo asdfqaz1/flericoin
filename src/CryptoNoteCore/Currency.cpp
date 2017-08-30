e875e5829ec7a6b02dc15dade116db7430bd7f30
Asdfqaz <shaye.garg@gmail.com>
2017-08-30 20:39:36 +0530

    Fixed bugs

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -99,6 +99,7 @@ bool Currency::generateGenesisBlock() {
 bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64_t alreadyGeneratedCoins,
   uint64_t fee, uint64_t& reward, int64_t& emissionChange) const {
   assert(alreadyGeneratedCoins <= m_moneySupply);
+    int baseReward;
     if(alreadyGeneratedCoins < 200000000000000) {
         baseReward = 50000000;
     } else if(alreadyGeneratedCoins < 500000000000000) {
